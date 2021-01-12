20553e59c331d632c837ee14208a9674147f44f1
Jaime Blandon <jblandon@presidencia.gob.sv>
2021-01-12 15:31:17 -0600

    my emacs conf v 0.1

---
+++ init.el
@@ -0,0 +1,54 @@
+;; Quitando el mensaje por defecto en la pantalla de inicio y los menus
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1) ;; Disable visible scrollbar
+(tool-bar-mode -1)   ;; Disable the toolbar
+(tooltip-mode -1)    ;; Disable tooltips
+(set-fringe-mode 10) ;: Give some breathing room
+
+(menu-bar-mode -1)   ;; Disable the menu bar
+
+;; set up the visible bell
+(setq visible-bell t)
+
+(load-theme 'tango-dark)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<scape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non linux-platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+;; IVY
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))

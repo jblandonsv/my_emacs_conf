399ca2105ce16a9d65238d06a214298167c5ce3c
Jaime Blandon <jblandon@presidencia.gob.sv>
2021-01-13 15:17:37 -0600

    agregando magit

--- init.el
+++ init.el
@@ -184,3 +184,12 @@
 
 
 ;; install ripgrep to search C-c p s r
+
+;; MAGIT
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :after magit)

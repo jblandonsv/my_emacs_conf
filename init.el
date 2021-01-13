9386d1054b10bc2938c96967e8d32ed640fe9a5a
Jaime Blandon <blandon.jaime@gmail.com>
2021-01-12 21:53:47 -0600

    config 1.5

--- init.el
+++ init.el
@@ -8,7 +8,16 @@
 (menu-bar-mode -1)   ;; Disable the menu bar
 
 ;; Colocando numeros de linea
-(global-linum-mode)
+;; (global-linum-mode)
+;; Colocando numeros de linea en el buffer
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda ()(display-line-numbers-mode 0))))
 
 ;; set up the visible bell
 ;; (setq visible-bell t)
@@ -59,3 +68,27 @@
   :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Which key
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+;; ivy rich + counsel
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; dont start searches with
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+

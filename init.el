5e4dda762b990c143eaa48fa9fdb08a2b69b6747
Jaime Blandon <blandon.jaime@gmail.com>
2021-01-15 06:26:20 -0600

    lsp-mode typescript + breadcrumbs headers

--- init.el
+++ init.el
@@ -326,9 +326,15 @@
   :config (counsel-projectile-mode))
 
 
+
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
 ;; LSP MODE !
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
+  :hook (lsp-mode . efs/lsp-mode-setup)
   :init
   (setq lsp-keymap-prefix "C-c l")
   :config
